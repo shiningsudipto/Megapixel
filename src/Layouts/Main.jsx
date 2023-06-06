04dca06022f8ab3e7284a25f1ed7a503d638671d
Sudipto Das <shiningsudipto@gmail.com>
2023-06-07 01:42:19 +0600

    Navbar added

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Shared/Navbar";
 
 const Main = () => {
     return (
         <div>
+            <Navbar />
             <Outlet />
         </div>
     );
