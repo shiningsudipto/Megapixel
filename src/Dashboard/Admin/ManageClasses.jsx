ab7db50f1507cd5229c40cff3ecd4e16a52dbc86
Sudipto Das <shiningsudipto@gmail.com>
2023-06-10 18:13:41 +0600

    added manage users and classes

---
+++ src/Dashboard/Admin/ManageClasses.jsx
@@ -0,0 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../Hook/useAxiosSecure";
+import MyClasses from "../../components/MyClasses";
+
+const ManageClasses = () => {
+    const [axiousSecure] = useAxiosSecure();
+    const { data: allClasses = [], refetch } = useQuery(['allClasses'],
+        async () => {
+            const res = await axiousSecure.get('/manageClasses');
+            return res.data;
+        }
+    )
+
+
+
+    console.log("All Classes:", allClasses);
+    return (
+        <div>
+            <div className="text-center">
+                <h2 className="text-3xl font-bold text-fuchsia-500 text-center">Manage Classes</h2>
+                <h3 className="text-xl font-semibold">Total Class: <span className="text-fuchsia-500">{allClasses.length}</span></h3>
+            </div>
+            <div className="grid grid-cols-2 gap-8">
+                {
+                    allClasses.map(singleClass => <MyClasses
+                        key={singleClass._id}
+                        singleClass={singleClass}
+                        refetch={refetch}
+                    ></MyClasses>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default ManageClasses;
